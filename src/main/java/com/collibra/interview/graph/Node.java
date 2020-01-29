a57ec35479df0496b3073cde44c68e2789a15f79
Mateusz Wieczorek <wskmateusz@gmail.com>
2020-01-29 20:06:58 +0100

    Improved Node comparison function

--- src/main/java/com/collibra/interview/graph/Node.java
+++ src/main/java/com/collibra/interview/graph/Node.java
@@ -3,12 +3,12 @@ package com.collibra.interview.graph;
 import lombok.Value;
 
 @Value
-public class Node implements Comparable {
+public class Node implements Comparable<Node> {
 
     private final String name;
 
     @Override
-    public int compareTo(Object o) {
-        return name.compareTo(o.toString());
+    public int compareTo(Node node) {
+        return name.compareTo(node.getName());
     }
 }
